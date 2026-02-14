49b071891c004e9574537dcb77bd8c789d76f9cb
Fangzhou Liu <evfangliu@gmail.com>
2026-02-13 18:33:05 -0800

    add info

--- ports/dbservice/portfile.cmake
+++ ports/dbservice/portfile.cmake
@@ -1,23 +1,24 @@
 # portfile.cmake
 include(vcpkg_common_functions)
 
-# Point to the local project directory
-vcpkg_from_path(
-    OUT_SOURCE_PATH SOURCE_PATH
-    PATH "C:\Users\evfan\source\repos\Calibration\DBService"
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO northwindtraders/beicode
+  REF 19a1f95c2f56a27ced90227b5e2754a602a08e69
+  SHA512 7b2bb7acb2a8ff07bff59cfa27247a7b2cced03828919cd65cc0c8cf1f724f5f1e947ed6992dcdbc913fb470694a52613d1861eaaadbf8903e94eb9cdfe4d000
+  HEAD_REF main
 )
 
-# Configure and build with CMake
 vcpkg_configure_cmake(
-    SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
 )
-
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 
-# Remove debug include files if not needed
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# Install license file
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)   
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
